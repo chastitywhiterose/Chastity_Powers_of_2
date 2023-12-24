0f907d0de809b440a55225d1efe0e2cb4daa540d
Chastity White Rose <chastitywhiterose@gmail.com>
2023-12-24 05:37:51 -0600

    Add files via upload

--- source/c/malloc/main.c
+++ source/c/malloc/main.c
@@ -1,1 +1,48 @@
+/* gcc -Wall -ansi -pedantic main.c -o main && ./main */
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+ int x,y,z;
+ int length=1000; 
+ int length2=1;
+ char *a=(char*)malloc(length*sizeof(*a));if(a==NULL){printf("memory allocation failed\n");return(1);}
+ 
+ x=0;
+ while(x<length)
+ {
+  a[x]=0;
+  x++;
+ }
+ a[0]=1;
+
+ z=0;
+ while(z<=256)
+ {
+
+  x=length2;
+  while(x>0)
+  {
+   x--;
+   printf("%i",a[x]);
+  }
+  printf("\n");
+
+  y=0;
+  x=0;
+  while(x<=length2)
+  {
+   a[x]+=a[x];
+   a[x]+=y;
+   if(a[x]>9){y=1;a[x]-=10;}else{y=0;}
+   x++;
+  }
+  if(a[length2]>0){length2++;}
+
+  z++;
+ }
+
+ free(a);
+ return 0;
+}
 

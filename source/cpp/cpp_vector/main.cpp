622a2e3d838636f464c36577a7294345d0f97784
Chastity White Rose <chastitywhiterose@gmail.com>
2023-12-24 07:13:36 -0600

    Add files via upload

--- source/cpp/cpp_vector/main.cpp
+++ source/cpp/cpp_vector/main.cpp
@@ -1,1 +1,39 @@
-fff
+/* g++ -Wall -ansi -pedantic main.cpp -o main && ./main */
+#include <stdio.h>
+#include <vector>
+int main()
+{
+ unsigned int x,y,z;
+
+ std::vector<int> a;
+ 
+ a.push_back(1);
+
+ z=0;
+ while(z<=256)
+ {
+
+  x=a.size();
+  while(x>0)
+  {
+   x--;
+   printf("%i",a[x]);
+  }
+  printf("\n");
+
+  y=0;
+  x=0;
+  while(x<a.size())
+  {
+   a[x]+=a[x];
+   a[x]+=y;
+   if(a[x]>9){y=1;a[x]-=10;}else{y=0;}
+   x++;
+  }
+  if(y>0){a.push_back(y);}
+
+  z++;
+ }
+
+ return 0;
+}
